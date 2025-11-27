d1f3c9e7e3c231d17052b886b386a9183138216a
Shivang572 <kotharishivang50@gmail.com>
2025-11-27 12:48:24 +0530

    Refactor Streamlit app layout and input handling

--- app1.py
+++ app1.py
@@ -1,36 +1,75 @@
 import streamlit as st
 import pickle
+import numpy as np
+
 
 pipe = pickle.load(open('pipe.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
 
 st.title("Laptop Predictor")
-#brand
-company = st.selectbox('Brand',df['Company'].unique())
-#type of user
-type = st.selectbox('Type',df['TypeName'].unique())
-#Ram
-ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
 
-#weight
+# brand
+company = st.selectbox('Brand', df['Company'].unique())
+
+# type of user
+type = st.selectbox('Type', df['TypeName'].unique())
+
+# RAM
+ram = st.selectbox('RAM(in GB)', [2,4,6,8,12,16,24,32,64])
+
+# weight
 weight = st.number_input("Weight (in kg)")
-#touchscreen
-touchscreen = st.selectbox('TouchScreen',['Yes','No'])
-#ips
-ips = st.selectbox('IPS',['YES','NO'])
-#screensize
+
+# touchscreen
+touchscreen = st.selectbox('TouchScreen', ['Yes','No'])
+
+# ips
+ips = st.selectbox('IPS', ['Yes','No'])
+
+# screen size
 screen_size = st.number_input('Screen Size')
-#resolution
-resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
-#cpu
-cpu = st.selectbox('CPU',df['Cpu_Brand'].unique())
-#hdd
-hdd =   st.selectbox('HDD(In GB)',[0,128,256,512,1024,2048])
-ssd =   st.selectbox('SSD(In GB)',[0,8,128,256,512,1024])
 
-gpu = st.selectbox('GPU',df['Gpu_brand'].unique())
+# resolution
+resolution = st.selectbox('Screen Resolution', [
+    '1920x1080','1366x768','1600x900','3840x2160','3200x1800',
+    '2880x1800','2560x1600','2560x1440','2304x1440'
+])
 
-os = st.selectbox('OS',df['os'].unique())
+# cpu
+cpu = st.selectbox('CPU', df['Cpu_Brand'].unique())
+
+# hdd / ssd
+hdd = st.selectbox('HDD(In GB)', [0,128,256,512,1024,2048])
+ssd = st.selectbox('SSD(In GB)', [0,8,128,256,512,1024])
+
+# gpu
+gpu = st.selectbox('GPU', df['Gpu_brand'].unique())
+
+# os
+os = st.selectbox('OS', df['os'].unique())
 
 if st.button('Predict Price'):
-    pass
+
+    # Prevent division by zero
+    if screen_size == 0:
+        st.error("Please enter a valid screen size (cannot be 0).")
+    else:
+        # convert Yes/No → 1/0
+        touchscreen = 1 if touchscreen == 'Yes' else 0
+        ips = 1 if ips == 'Yes' else 0
+
+        # PPI calculation
+        X_res = int(resolution.split('x')[0])
+        Y_res = int(resolution.split('x')[1])
+        ppi = ((X_res**2 + Y_res**2) ** 0.5) / screen_size
+
+        # Form query
+        query = np.array([
+            company, type, ram, weight, touchscreen,
+            ips, ppi, cpu, hdd, ssd, gpu, os
+        ]).reshape(1, 12)
+
+        # Predict
+        pred = int(np.exp(pipe.predict(query)[0]))
+
+        st.title(f"Predicted Price: ₹{pred}")
